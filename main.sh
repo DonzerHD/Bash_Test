55cbbbf7f6ae7f236ff295a150cc215e434cdd23
DonzerHD <thomas.lemay59@outlook.fr>
2023-03-09 15:59:42 +0100

    add var

--- main.sh
+++ main.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "Hello World"
+# Commande read :
+# p - ajouter un texte avant la saisie
+# s - masquer la saisie
+# n - limiter à N caractères lus
+# t - limiter la saisie pendant N secondes
+
+read -p "Enter your name :" -t 2 name
+echo -e "\n Saisie : $name"
