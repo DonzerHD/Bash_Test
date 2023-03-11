833448290a55d1ee7567a64e19b5467109aac500
DonzerHD <thomas.lemay59@outlook.fr>
2023-03-11 11:34:44 +0100

    operateur

---
+++ main_2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Commande declare :
+# r(lecture seule)
+# i(numérique)
+# a
+# etc...
+
+# $# : nombre de paramètres
+# $0 : exécutable
+# $1  à $9 : paramètres 1 à 9
+# ${X} : paramètre X
+# $$ , $# , $* , $@
+
+#let  'number = 145 + 5'
+
+result=$((34 + 33))
+echo $result
