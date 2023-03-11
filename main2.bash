bce2f272b3f3bd520303ecdab6739611c0693674
DonzerHD <thomas.lemay59@outlook.fr>
2023-03-11 14:22:25 +0100

    boucle

---
+++ main2.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# path=`pwd`
+
+# if [ $path == "/home/thomas59/Bash_Test" ]; then
+#     echo "You are in the home directory"
+# else
+#     echo "You are not in the home directory"
+# fi
+
+# i=0
+# while ((i < 10)) # i >= 10
+# do
+#    echo $i
+#    i=$(($i+1))
+# done
+
+# ((intialisations ; test ; incrementation))
+
+for ((i = 0 ; i < 10 ; i++))
+do
+    echo $i
+done
